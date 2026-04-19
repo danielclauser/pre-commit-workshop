cb9fbb9ed768b0f61e699167a099b426417be01b
dclauser <clauser.daniel@gmail.com>
2026-04-19 15:23:18 +0200

    added numpydoc config

--- example.py
+++ example.py
@@ -1,3 +1,25 @@
+"My docstring."
+
+
 def my_function(a):
-    """My function."""
+    """
+    My function.
+
+    Parameters
+    ----------
+    a : int
+        Example variable.
+    """
+    pass
+
+
+def try_another(c):
+    """
+    Try another.
+
+    Parameters
+    ----------
+    c : int
+        Example variable.
+    """
     pass
